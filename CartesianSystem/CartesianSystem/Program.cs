26e7c8184391621dde61e4ac0d822c3a914cd9a6
asmithasatesh <asmithasatesh@gmail.com>
2021-06-28 10:35:00 +0530

    Adding Cartesian System Project

---
+++ CartesianSystem/CartesianSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CartesianSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
